0c4c34ddc883f45f8ea9bc7699a7bd0c0ee43676
fa-reyes <fabian.andree2004@gmail.com>
2026-05-15 13:22:01 -0400

    New CSS added and modified. Logic in JS fixed

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,11 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    hamButton.classList.toggle("open");
+    navigation.classList.toggle("open");
+})
